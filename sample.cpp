d6847ec446451fead21729b459459ce9002f9040
Aneesh1856 <aneesh.dhavade@s.amity.edu>
2025-02-17 11:25:13 +0530

    first commit

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     printf("Size of int: %lu bytes\n", sizeof(int));
     printf("Size of float: %lu bytes\n", sizeof(float));
     printf("Size of double: %lu bytes\n", sizeof(double));
